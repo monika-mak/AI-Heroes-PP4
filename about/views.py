0a904094a55896ed6e8f079c3390906fba339efb
Monika Mak <monika1986mak@gmail.com>
2024-11-01 14:10:05 +0000

    create functional communication request form sent to the admin

--- about/views.py
+++ about/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib import messages
 from .models import About
 from .forms import CommunicationForm
 
@@ -8,6 +9,16 @@ def about_me(request, *args, **kwargs):
     """
     About page rendering
     """
+    if request.method == "POST":
+        communication_form = CommunicationForm(data=request.POST)
+        if communication_form.is_valid():
+            communication_form.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                "Collaboration request received! I endeavor to respond within 2 working days."
+            )
+
+
     about = About.objects.all().order_by('-updated_on').first()
     communication_form = CommunicationForm()
 
