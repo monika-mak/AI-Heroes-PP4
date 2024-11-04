99c77985bd2789d2ee40db5e5c7da59e9c34478c
Monika Mak <monika1986mak@gmail.com>
2024-11-04 17:03:44 +0000

    change images to posts

--- ai_heroes/settings.py
+++ ai_heroes/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-monikamak-aiheroespp4-y4ys4ixf98g.ws.codeinstitute-ide.net','.herokuapp.com']
 
@@ -148,6 +148,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+# STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
