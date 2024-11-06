bd3efdc28d9173f3179e37705178fe0fc3b6fe76
Monika Mak <monika1986mak@gmail.com>
2024-11-06 02:17:39 +0000

    add posts to the leaderboard, fix issue with url paths

--- blog/urls.py
+++ blog/urls.py
@@ -4,9 +4,10 @@ from .views import Leaderboard
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
+    path('leaderboard/', Leaderboard.as_view(), name='leaderboard'),
     path('<slug:slug>/', views.post_detail, name='post_detail'),
     path('<slug:slug>/edit_comment/<int:comment_id>', views.comment_edit, name='comment_edit'),
     path('<slug:slug>/delete_comment/<int:comment_id>', views.comment_delete, name='comment_delete'),
-    path('/leaderboard/', Leaderboard.as_view(), name='leaderboard')
+    
 ]
 
--- blog/views.py
+++ blog/views.py
@@ -14,12 +14,23 @@ class PostList(generic.ListView):
 
 
 def post_detail(request, slug):
+    """
+    Display an individual :model:`blog.Post`.
+
+    **Context**
+
+    ``post``
+        An instance of :model:`blog.Post`.
+
+    **Template:**
+
+    :template:`blog/post_detail.html`
+    """
     queryset = Post.objects.filter(status=1)
     post = get_object_or_404(queryset, slug=slug)
     comments = post.comments.all().order_by("-created_on")
     comment_count = post.comments.filter(approved=True).count()
     if request.method == "POST":
-        print("Received a POST request")
         comment_form = CommentForm(data=request.POST)
         if comment_form.is_valid():
             comment = comment_form.save(commit=False)
@@ -30,17 +41,17 @@ def post_detail(request, slug):
                 request, messages.SUCCESS,
                 'Comment submitted and awaiting approval'
             )
-
+    
     comment_form = CommentForm()
-    print("About to render template")
 
     return render(
-        request, "blog/post_detail.html",
+        request,
+        "blog/post_detail.html",
         {
             "post": post,
             "comments": comments,
             "comment_count": comment_count,
-            "comment_form": comment_form,
+            "comment_form": comment_form
         },
     )
 
