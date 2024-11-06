a16d7694f50a2b3529b5719db93abe15f4ac1446
Monika Mak <monika1986mak@gmail.com>
2024-11-06 01:13:21 +0000

    add leaderboard url path

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,12 @@
 from . import views
 from django.urls import path
+from .views import Leaderboard
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path('<slug:slug>/', views.post_detail, name='post_detail'),
     path('<slug:slug>/edit_comment/<int:comment_id>', views.comment_edit, name='comment_edit'),
     path('<slug:slug>/delete_comment/<int:comment_id>', views.comment_delete, name='comment_delete'),
+    path('/leaderboard/', Leaderboard.as_view(), name='leaderboard')
 ]
 
