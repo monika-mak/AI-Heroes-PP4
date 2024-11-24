f5e3108e706a99f024f3b4c0e06f7f833f696d8b
Monika Mak <monika1986mak@gmail.com>
2024-11-24 02:04:11 +0000

    add comments and doc str ,clean code , validated with pep8

--- about/models.py
+++ about/models.py
@@ -2,8 +2,10 @@ from django.db import models
 from cloudinary.models import CloudinaryField
 
 
-# Create your models here.
 class About(models.Model):
+    """
+    Represents the 'About' section of a website.
+    """
     title = models.CharField(max_length=200)
     profile_image = CloudinaryField('image', default='placeholder')
     updated_on = models.DateTimeField(auto_now=True)
@@ -14,10 +16,14 @@ class About(models.Model):
 
 
 class CommunicationRequest(models.Model):
+    """
+    Represents a communication request submitted by a user.
+    """
     name = models.CharField(max_length=200)
     email = models.EmailField()
     message = models.TextField()
-    read = models.BooleanField(default=False)
+    # Track if the request has been addressed
+    read = models.BooleanField(default=False)  
 
     def __str__(self):
         return f"Communication request from {self.name}"
