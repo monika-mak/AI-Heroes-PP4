42e9055779d3fad9053cefa98a09e97516b850ac
Monika Mak <monika1986mak@gmail.com>
2024-11-27 23:10:30 +0000

    remove unnecesary css,change back the color font

--- ai_heroes/settings.py
+++ ai_heroes/settings.py
@@ -157,9 +157,8 @@ STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-# STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
-# code modeled from  
+# code modeled from  https://github.com/amylour/FreeFido_v2/blob/main/freefido/settings.py
 MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
